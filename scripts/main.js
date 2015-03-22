ddaa0c125c1a1d0dd62e5e396cd50fe331a6ba9b
atanderson <andrew.anderson@crystalcommerce.com>
2015-03-21 18:21:17 -0700

    Circle draw attempt
    Currently non-functional attempt at drawing a circle of static that
    follows the cursor.

--- scripts/main.js
+++ scripts/main.js
@@ -4,30 +4,50 @@ canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d"),
 imgData = ctx.createImageData(canvas.width, canvas.height);
 
-console.log(canvas.height, canvas.width, 'width and height');
-console.log(window.innerWidth, canvas.width);
-
 var drawStatic = function (mousePosition) {
+    var width = 200;
+    var imgData = ctx.createImageData(width, width);
 
-    //console.log(mousePosition.x);
-    imgData = ctx.createImageData(50, 50);
+    // for (i = 0 ; i < imgData.data.length ; i+=4 ){
+    //     var randomColor = Math.floor(Math.random()*100)
+    //     imgData.data[i+0] = randomColor;
+    //     imgData.data[i+1] = randomColor;
+    //     imgData.data[i+2] = randomColor;
+    //     imgData.data[i+3] = 255;
+    // }
 
-    for (i = 0 ; i < imgData.data.length ; i+=4 ){
-        var randomColor = Math.floor(Math.random()*256)
+    var width   = 200,
+        height  = width,
+        radius  = width/2,
+        centerX = width/2,
+        centerY = height/2;
+
+    var crinkleDatPixelAt = function(i){
+        var randomColor = Math.floor(Math.random()*100)
         imgData.data[i+0] = randomColor;
         imgData.data[i+1] = randomColor;
         imgData.data[i+2] = randomColor;
         imgData.data[i+3] = 255;
     }
 
-    ctx.putImageData(imgData, mousePosition.x, mousePosition.y);
+    for (var row = 0; row < radius; row++){
+        //these values could be cached in an array where the index was row:
+        // and row might need to be corrected since it comes from the top, with something like height-row, but it could also work fine
+        var xStart = Math.sqrt(Math.pow(radius,2) - Math.pow(row-centerY, 2) + centerX),
+            xEnd   = width - xStart;
+        for(var x = xStart; x <= xEnd; x++){
+            crinkleDatPixelAt(row * width * 4 + x * 4);
+        }
+    }
+
+    ctx.putImageData(imgData, mousePosition.x - 100, mousePosition.y - 100);
 }
 
 var drawInitialStatic = function(){
-    imgData = ctx.createImageData(canvas.width, canvas.height);
+    var imgData = ctx.createImageData(canvas.width, canvas.height);
 
-    for (i = 0 ; i < imgData.data.length ; i+=4 ){
-        var randomColor = Math.floor(Math.random()*256)
+    for (var i = 0 ; i < imgData.data.length ; i+=4 ){
+        var randomColor = Math.floor(Math.random()*100)
         imgData.data[i+0] = randomColor;
         imgData.data[i+1] = randomColor;
         imgData.data[i+2] = randomColor;
@@ -49,7 +69,7 @@ var getMousePosition = function (canvas, e) {
 var refresh = function (e) {
     var pos = getMousePosition(canvas, e);
     window.requestAnimationFrame(function () {
-        drawStatic(pos)
+        drawStatic(pos);
     });
 }
 
