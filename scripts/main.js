6e86063674068c70eaa11ecdb04b7d5bb5c856ae
atanderson <andrew.anderson@crystalcommerce.com>
2015-03-22 14:27:52 -0700

    Fixed circle draw, canvas removed
    Fixed the circle draw/paint effect by building rows as individual
    imgData.
    Removed the extra canvas.
    
    :palm_tree: :sun_with_face: :palm_tree:

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
-var bgCanvas = document.getElementById('canvas');
-var topCanvas = document.getElementById('canvas1');
-var ctx = bgCanvas.getContext("2d");
-var topCtx = topCanvas.getContext("2d");
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext("2d");
 
 var crinkleDatPixelAt = function(data, max, i){
     var randomColor = Math.floor(Math.random()*max);
@@ -9,7 +7,8 @@ var crinkleDatPixelAt = function(data, max, i){
     data[i+1] = randomColor;
     data[i+2] = randomColor;
     data[i+3] = 255;
-}
+};
+
 var drawStatic = (function(){
     var width     = 200,
         trailSize = width*2/3,
@@ -29,44 +28,33 @@ var drawStatic = (function(){
         endBackgroundMax   = 110,
         backgroundColorMax = initBackgroundMax,
         backroundDirection = 0.5;
-
-    return function (mousePosition) {
-        topCtx.clearRect(0, 0, topCanvas.width, topCanvas.height);
-        var imgData = topCtx.createImageData(width, width);
-
-        for (var row = 0; row <= height; row++) {
-            var xEnd   = xEndByRow[row],
-                xStart = -xEnd + 0;
-            for(var x = xStart - radius; x <= xEnd - radius; x++){
-                var offset = (row * width * 4) + (x * 4);
-                crinkleDatPixelAt(imgData.data, colorMax, offset);
-            }
-        }
-        topCtx.putImageData(imgData, mousePosition.x - radius, mousePosition.y - radius);
-
-        //draw a box in the lower context to make a "trail" like effect
-        lowerImg = ctx.createImageData(trailSize, trailSize);
-
         backgroundColorMax += backroundDirection;
         if(backgroundColorMax > endBackgroundMax || backgroundColorMax < initBackgroundMax){
             backroundDirection *= -1;
         }
 
-        for (i = 0 ; i < lowerImg.data.length ; i+=4 ){
-            crinkleDatPixelAt(lowerImg.data, backgroundColorMax, i);
-        }
-        ctx.putImageData(lowerImg, mousePosition.x-(trailSize/2), mousePosition.y-(trailSize/2));
+    return function (mousePosition) {
+      
+        for (var row = 0; row <= height; row++) {
+            var xEnd   = xEndByRow[row],
+                xStart = -xEnd + 0;
+            if (xEnd != 0 && xStart != 0){
+                var imgData = ctx.createImageData((Math.abs(xStart) + Math.abs(xEnd)), 1 );
+                for(var i = 0; i <= imgData.data.length ; i+= 4){
+                    crinkleDatPixelAt(imgData.data, backgroundColorMax, i)
+                }
+                ctx.putImageData(imgData, mousePosition.x - (imgData.data.length / 8), mousePosition.y + row - radius);
+            }
 
-    }
+        }
+        
+    };
 }());
 
-
 var drawInitialStatic = function(){
-    bgCanvas.width = window.innerWidth;
-    bgCanvas.height = window.innerHeight;
-    topCanvas.width = window.innerWidth;
-    topCanvas.height = window.innerHeight;
-    var imgData = ctx.createImageData(bgCanvas.width, bgCanvas.height);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var imgData = ctx.createImageData(canvas.width, canvas.height);
 
     for (var i = 0 ; i < imgData.data.length ; i+=4 ){
         crinkleDatPixelAt(imgData.data, 100, i);
@@ -74,7 +62,7 @@ var drawInitialStatic = function(){
 
     ctx.putImageData(imgData, 0, 0);
 
-}
+};
 
 var getMousePosition = function (canvas, e) {
     var rect = canvas.getBoundingClientRect();
@@ -82,7 +70,7 @@ var getMousePosition = function (canvas, e) {
         x: e.clientX - rect.left,
         y: e.clientY - rect.top
     };
-}
+};
 
 var refresh = function (e) {
     var pos = getMousePosition(canvas, e);
