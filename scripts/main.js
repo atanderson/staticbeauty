6e2a1a3a03ee0f12b42f848ca891295258cc5e53
atanderson <andrew.anderson@crystalcommerce.com>
2015-03-24 22:25:36 -0700

    Adding comments
    Commented most of the major functions and added a few questions for
    collaborators.

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,71 @@
+//Required to 'write' to the canvas
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext("2d");
 
+//Sets the data subset representing a pixel to a random greyscale value
 var crinkleDatPixelAt = function(data, max, i){
+
     var randomColor = Math.floor(Math.random()*max);
-    data[i+0] = randomColor;
-    data[i+1] = randomColor;
-    data[i+2] = randomColor;
-    data[i+3] = 255;
+    data[i+0] = randomColor; //Red
+    data[i+1] = randomColor; //Green
+    data[i+2] = randomColor; //Blue
+    data[i+3] = 255;         //Alpha
+
 };
 
+//Generates a circle of randomized pixels and writes it at the cursor position
 var drawStatic = (function(){
-    var width     = 200,
-        trailSize = width*2/3,
-        height    = width,
-        radius    = width/2,
-        centerX   = width/2,
-        centerY   = height/2,
-        colorMax  = 145,
-        xEndByRow = [];
 
-    //precalculate the xEnd for every row
+    //Define the circle. Units are pixels unless noted otherwise
+    //?ANDREW: do we need all these, some are redundant
+    var width     = 200,       
+        height    = width,     
+        radius    = width/2,   
+        centerX   = width/2,
+        centerY   = height/2;
+
+    //Starting from the top of the circle, determine now long the row of pixels
+    //should be using the circle equation 
+    var xEndByRow = [];
     for (var row = 0; row <= height; row++) {
-        xEndByRow[row] = Math.round(Math.sqrt(Math.pow(radius,2) - Math.pow(row-centerY, 2)) + 0);
+        xEndByRow[row] = Math.round(Math.sqrt(Math.pow(radius, 2) - Math.pow(row-centerY, 2)) + 0);
     }
 
+    //Brightness bounds used for oscillating static 'color' effect
     var initBackgroundMax  = 95,
         endBackgroundMax   = 110,
         backgroundColorMax = initBackgroundMax,
         backroundDirection = 0.5;
-        backgroundColorMax += backroundDirection;
-        if(backgroundColorMax > endBackgroundMax || backgroundColorMax < initBackgroundMax){
-            backroundDirection *= -1;
-        }
 
     return function (mousePosition) {
 
+        //Increment the pixel color bounds
+        backgroundColorMax += backroundDirection;
+        //If the color bounds exceeds the max, begin decrementing it
+        if(backgroundColorMax > endBackgroundMax || backgroundColorMax < initBackgroundMax){
+            backroundDirection *= -1;
+        }
+        
+        //Starting from the top of the circle, draw the row of pixels and place
+        //them so the circle is centered on the cursor.
         for (var row = 0; row <= height; row++) {
+
+            //Get the pre-set length of the row of pixels
             var xEnd   = xEndByRow[row],
-                xStart = -xEnd + 0;
+                xStart = -xEnd;
+
+            //?ANDREW can't remember why/if this if statement is necessary
             if (xEnd != 0 && xStart != 0){
-                var imgData = ctx.createImageData((Math.abs(xStart) + Math.abs(xEnd)), 1 );
+
+                //Create an imgData array that is 1 pixel tall
+                var imgData = ctx.createImageData(Math.abs(xStart) + xEnd, 1 );
+                //randomize every pixel within the imgData row
                 for(var i = 0; i <= imgData.data.length ; i+= 4){
                     crinkleDatPixelAt(imgData.data, backgroundColorMax, i)
                 }
+
+                //Draw the circle relative to the cursor. NOTE: we divide by 8
+                //to turn the clamped array into 'pixels' then find the center
                 ctx.putImageData(imgData, mousePosition.x - (imgData.data.length / 8), mousePosition.y + row - radius);
             }
 
@@ -51,9 +74,14 @@ var drawStatic = (function(){
     };
 }());
 
+//Draw a randomized canvas of pixels that is the size of the browser window
 var drawInitialStatic = function(){
+
+    //Dimensions of the browser window
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    //NOTE: Suspected to be taxing to generate
     var imgData = ctx.createImageData(canvas.width, canvas.height);
 
     for (var i = 0 ; i < imgData.data.length ; i += 4){
@@ -64,6 +92,7 @@ var drawInitialStatic = function(){
 
 };
 
+//ANDREW: This is experimental
 var drawRainbow = function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -121,6 +150,7 @@ var drawRainbow = function(){
     ctx.putImageData(imgData, 0, 0);
 }
 
+//Return the mouse position in the window
 var getMousePosition = function (canvas, e) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -129,6 +159,7 @@ var getMousePosition = function (canvas, e) {
     };
 };
 
+//Function used to bind drawStatic to recurring events
 var refresh = function (e) {
     var pos = getMousePosition(canvas, e);
     window.requestAnimationFrame(function () {
@@ -136,6 +167,7 @@ var refresh = function (e) {
     });
 }
 
+//ANDREW: This is experimental for the secret reveal effect
 var showSecret = function (e){
     var pos = getMousePosition(canvas, e);
     var width = window.innerWidth;
@@ -145,15 +177,17 @@ var showSecret = function (e){
         secretTrigger ++;
     }
     if (secretTrigger > 50){
-        console.log('reveal secret');
+        window.requestAnimationFrame(drawRainbow);
     }
 };
 
+//Draw the initial static texture for the page
 drawInitialStatic();
+
+//ANDREW: This is experimental for the secret reveal effect
 var secretTrigger = 0;
+
 window.addEventListener('mousemove', refresh, false);
 window.addEventListener('mousemove', showSecret, false);
 window.addEventListener('resize', drawInitialStatic, false);
-window.requestAnimationFrame(drawStatic);
 
-window.requestAnimationFrame(drawRainbow);
